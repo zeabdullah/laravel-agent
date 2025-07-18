686f318cab77835a65e11113e27ca74fb5b3c8ef
Abdullah Z <dev@zabd.dev>
2025-07-18 19:22:35 +0300

    update table `agents` to `the_agents` for Agent model

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Agent extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'the_agents';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'ai_id';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true; // we'd like to keep this true
+
+    /**
+     * The data type of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'int';
 }
--- database/migrations/2025_07_18_162028_create_the_agents_table.php
+++ database/migrations/2025_07_18_162028_create_the_agents_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('agents', function (Blueprint $table) {
-            $table->id();
+        Schema::create('the_agents', function (Blueprint $table) {
+            $table->id('ai_id');
             $table->timestamps();
         });
     }
@@ -22,6 +21,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('agents');
+        Schema::dropIfExists('the_agents');
     }
 };
